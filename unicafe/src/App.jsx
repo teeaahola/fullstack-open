1089561e877b008c652b7c13614c7066f059581d
Teea Ahola <teea.ahola@gmail.com>
2025-12-18 16:17:31 +0200

    1.11

--- unicafe/src/App.jsx
+++ unicafe/src/App.jsx
@@ -5,7 +5,12 @@ const Button = ({ onClick, text }) => {
 }
 
 const StatisticLine = ({ text, value }) => {
-  return <div>{text} {value}</div>
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  )
 }
 
 const Statistics = ({ good, neutral, bad }) => {
@@ -16,14 +21,16 @@ const Statistics = ({ good, neutral, bad }) => {
   }
 
   return (
-    <div>
-      <StatisticLine text="good" value={good} />
-      <StatisticLine text="neutral" value={neutral} />
-      <StatisticLine text="bad" value={bad} />
-      <StatisticLine text="all" value={all} />
-      <StatisticLine text="average" value={all ? (good - bad) / all : 0} />
-      <StatisticLine text="positive" value={all ? (good / all) * 100 + " %" : "0 %"} />
-    </div>
+    <table>
+      <tbody>
+        <StatisticLine text="good" value={good} />
+        <StatisticLine text="neutral" value={neutral} />
+        <StatisticLine text="bad" value={bad} />
+        <StatisticLine text="all" value={all} />
+        <StatisticLine text="average" value={all ? (good - bad) / all : 0} />
+        <StatisticLine text="positive" value={all ? (good / all) * 100 + " %" : "0 %"} />
+      </tbody>
+    </table>
   )
 }
 
