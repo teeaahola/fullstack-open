764b3689c928efa54c8225ff981142b64f4cac30
Teea Ahola <teea.ahola@gmail.com>
2025-12-21 19:02:23 +0200

    3.3

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -32,6 +32,16 @@ app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  const person = persons.find(person => person.id === id)
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
 app.get('/info', (request, response) => {
   const date = new Date()
   response.send(`<p>Phonebook has info for ${persons.length} people</p><p>${date}</p>`)
