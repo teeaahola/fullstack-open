8ff5659a18dde5ee40be695394d8fd59b131fc73
Teea Ahola <teea.ahola@gmail.com>
2025-12-21 19:49:27 +0200

    3.6

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -59,9 +59,21 @@ const generateId = () => {
 app.post('/api/persons', (request, response) => {
   const body = request.body
 
-  if (!body.name || !body.number) {
+  if (!body.name) {
     return response.status(400).json({
-      error: 'content missing'
+      error: 'name missing'
+    })
+  }
+
+  if (!body.number) {
+    return response.status(400).json({
+      error: 'number missing'
+    })
+  }
+
+  if (persons.find(person => person.name === body.name)) {
+    return response.status(400).json({
+      error: 'name must be unique'
     })
   }
 
