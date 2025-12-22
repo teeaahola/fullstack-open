d46c31c78e3f617bf97f93a33e8bcc59086e8271
Teea Ahola <teea.ahola@gmail.com>
2025-12-22 21:50:57 +0200

    3.14

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -37,21 +37,14 @@ app.post('/api/persons', (request, response) => {
     })
   }
 
-  if (persons.find(person => person.name === body.name)) {
-    return response.status(400).json({
-      error: 'name must be unique'
-    })
-  }
-
-  const person = {
+  const person = new Person({
     name: body.name,
     number: body.number,
-    id: generateId(),
-  }
+  })
 
-  persons = persons.concat(person)
-
-  response.json(person)
+  person.save().then(person => {
+    response.json(person)
+  })
 })
 
 const PORT = process.env.PORT
