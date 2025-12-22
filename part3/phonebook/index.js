23e78bcba9306906d93fb972351d43bedc2d3ecc
Teea Ahola <teea.ahola@gmail.com>
2025-12-22 21:45:21 +0200

    3.13

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,47 +1,18 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
-app.use(express.static('dist'))
+const Person = require('./models/person')
 
+app.use(express.static('dist'))
 app.use(express.json())
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-let persons = [
-  {
-    "id": "1",
-    "name": "Arto Hellas",
-    "number": "040-123456"
-  },
-  {
-    "id": "2",
-    "name": "Ada Lovelace",
-    "number": "39-44-5323523"
-  },
-  {
-    "id": "3",
-    "name": "Dan Abramov",
-    "number": "12-43-234345"
-  },
-  {
-    "id": "4",
-    "name": "Mary Poppendieck",
-    "number": "39-23-6423122"
-  }
-]
-
 app.get('/api/persons', (request, response) => {
-  response.json(persons)
-})
-
-app.get('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  const person = persons.find(person => person.id === id)
-  if (person) {
-    response.json(person)
-  } else {
-    response.status(404).end()
-  }
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
 })
 
 app.delete('/api/persons/:id', (request, response) => {
@@ -50,11 +21,6 @@ app.delete('/api/persons/:id', (request, response) => {
 
   response.status(204).end()
 })
-
-const generateId = () => {
-  const id = Math.floor(Math.random() * 100000 + 1)
-  return String(id)
-}
 
 app.post('/api/persons', (request, response) => {
   const body = request.body
@@ -88,7 +54,7 @@ app.post('/api/persons', (request, response) => {
   response.json(person)
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
