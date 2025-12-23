0757d3fa37b8ac0f77ec5c702a414ca8f4fc73ce
Teea Ahola <teea.ahola@gmail.com>
2025-12-23 10:47:19 +0200

    3.15

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,13 +1,14 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
-const morgan = require('morgan')
-const Person = require('./models/person')
-
 app.use(express.static('dist'))
 app.use(express.json())
+const morgan = require('morgan')
+
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
+const Person = require('./models/person')
 
 app.get('/api/persons', (request, response) => {
   Person.find({}).then(persons => {
@@ -15,11 +16,22 @@ app.get('/api/persons', (request, response) => {
   })
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  persons = persons.filter(person => person.id !== id)
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+})
 
-  response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
 })
 
 app.post('/api/persons', (request, response) => {
