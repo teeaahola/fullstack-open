d2c420b1419b99450676a330fab140de47d31f48
Teea Ahola <teea.ahola@gmail.com>
2025-12-28 12:26:39 +0200

    controller middleware

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -3,25 +3,10 @@ const logger = require('./utils/logger')
 const express = require('express')
 
 const app = express()
-
-const Blog = require('./models/blog')
-
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog.save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+const blogRouter = require('./controllers/blog')
+app.use('/api/blogs', blogRouter)
 
 const PORT = config.PORT || 3003
 app.listen(PORT, () => {
