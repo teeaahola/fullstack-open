2c747251e71a7172b42f1c9cab062731d18b22c0
Teea Ahola <teea.ahola@gmail.com>
2025-12-29 23:04:22 +0200

    4.9-4.12

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -3,59 +3,20 @@ const assert = require('node:assert')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
+const helper = require('./test_helper')
 const Blog = require('../models/blog')
-const blog = require('../models/blog')
 
 const api = supertest(app)
 
-const blogs = [
-  {
-    title: "React patterns",
-    author: "Michael Chan",
-    url: "https://reactpatterns.com/",
-    likes: 7
-  },
-  {
-    title: "Go To Statement Considered Harmful",
-    author: "Edsger W. Dijkstra",
-    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-    likes: 5
-  },
-  {
-    title: "Canonical string reduction",
-    author: "Edsger W. Dijkstra",
-    url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
-    likes: 12
-  },
-  {
-    title: "First class tests",
-    author: "Robert C. Martin",
-    url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
-    likes: 10
-  },
-  {
-    title: "TDD harms architecture",
-    author: "Robert C. Martin",
-    url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
-    likes: 0
-  },
-  {
-    title: "Type wars",
-    author: "Robert C. Martin",
-    url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
-    likes: 2
-  }
-]
-
 beforeEach(async () => {
   await Blog.deleteMany({})
-  for (let i = 0; i < blogs.length; i++) {
-    let blogObject = new Blog(blogs[i])
+  for (let i = 0; i < helper.initialBlogs.length; i++) {
+    let blogObject = new Blog(helper.initialBlogs[i])
     await blogObject.save()
   }
 })
 
-test.only('blogs are returned as json', async () => {
+test('blogs are returned as json', async () => {
   await api
     .get('/api/blogs')
     .expect(200)
@@ -65,14 +26,95 @@ test.only('blogs are returned as json', async () => {
 test('all blogs are returned', async () => {
   const response = await api.get('/api/blogs')
 
-  assert.strictEqual(response.body.length, blogs.length)
+  assert.strictEqual(response.body.length, helper.initialBlogs.length)
 })
 
 test('a specific blog is within the returned blogs', async () => {
   const response = await api.get('/api/blogs')
 
-  const titles = response.body.map(e => e.title)
+  const titles = response.body.map(b => b.title)
   assert(titles.includes('First class tests'))
+})
+
+test('blogs have a unique identifier property named "id"', async () => {
+  const response = await api.get('/api/blogs')
+
+  const keys = Object.keys(response.body[0])
+  assert(keys.includes('id'))
+})
+
+test('a valid blog can be added', async () => {
+  const newBlog = {
+    title: 'async/await simplifies making async calls',
+    author: 'test',
+    url: 'test.url',
+    likes: 0
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const blogsAtEnd = await helper.blogsInDb()
+  assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1)
+
+  const titles = blogsAtEnd.map(b => b.title)
+
+  assert(titles.includes('async/await simplifies making async calls'))
+})
+
+test('a blog added without likes has zero likes', async () => {
+  const newBlog = {
+    title: 'async/await simplifies making async calls',
+    author: 'test',
+    url: 'test.url'
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const blogsAtEnd = await helper.blogsInDb()
+
+  const addedBlog = blogsAtEnd.find(b => b.author === 'test')
+
+  assert.equal(addedBlog.likes, 0)
+})
+
+test('a blog added without url responds with code 400', async () => {
+  const newBlog = {
+    title: 'async/await simplifies making async calls',
+    author: 'test',
+    likes: 0
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(400)
+
+  const blogsAtEnd = await helper.blogsInDb()
+  assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length)
+})
+
+test('a blog added without title responds with code 400', async () => {
+  const newBlog = {
+    author: 'test',
+    url: 'test.url',
+    likes: 0
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(400)
+
+  const blogsAtEnd = await helper.blogsInDb()
+  assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length)
 })
 
 after(async () => {
