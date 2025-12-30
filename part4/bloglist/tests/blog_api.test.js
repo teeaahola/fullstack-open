c8e686e7e1d750c26a50347e16aa058cf4022973
Teea Ahola <teea.ahola@gmail.com>
2025-12-30 19:23:55 +0200

    4.13

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -117,6 +117,22 @@ describe('when there are initially some blogs saved', () => {
       assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length)
     })
   })
+
+  describe('deletion of a blog', () => {
+    test('succeeds with status code 204 if id is valid', async () => {
+      const blogsAtStart = await helper.blogsInDb()
+      const blogToDelete = blogsAtStart[0]
+
+      await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204)
+
+      const blogsAtEnd = await helper.blogsInDb()
+
+      const titles = blogsAtEnd.map(n => n.title)
+      assert(!titles.includes(blogToDelete.title))
+
+      assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length - 1)
+    })
+  })
 })
 
 after(async () => {
