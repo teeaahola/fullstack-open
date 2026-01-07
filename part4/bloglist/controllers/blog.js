fc7879b2ab476849881956434b1dcf15a7f20e30
Teea Ahola <teea.ahola@gmail.com>
2026-01-07 16:01:38 +0200

    4.21

--- part4/bloglist/controllers/blog.js
+++ part4/bloglist/controllers/blog.js
@@ -35,7 +35,20 @@ blogRouter.post('/', async (request, response) => {
 })
 
 blogRouter.delete('/:id', async (request, response) => {
-  await Blog.findByIdAndDelete(request.params.id)
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
+
+  if (!user) {
+    return response.status(400).json({ error: 'UserId missing or not valid' })
+  }
+
+  const blog = await Blog.findById(request.params.id)
+  if (blog.user.toString() === decodedToken.id) {
+    await Blog.findByIdAndDelete(request.params.id)
+  }
   response.status(204).end()
 })
 
