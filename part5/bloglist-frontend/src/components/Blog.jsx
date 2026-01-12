ac8025a1f7a09467d1712ced5b162a6fcea2bf39
Teea Ahola <teea.ahola@gmail.com>
2026-01-12 23:36:17 +0200

    5.7

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,28 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+
+  const showWhenVisible = { display: visible ? '' : 'none' }
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  return (
+    <div className='blog'>
+      {blog.title} {blog.author}
+      <button onClick={toggleVisibility}>{visible ? 'hide' : 'view'}</button>
+      <div style={showWhenVisible}>
+        <div>{blog.url}</div>
+        <div>
+          {blog.likes}
+          <button onClick={() => console.log('like')}>like</button>
+        </div>
+        <div>{blog?.user?.username}</div>
+      </div>
+    </div>
+  )
+}
 
 export default Blog
